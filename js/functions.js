a43b6317f1c964cb37a39cb38c96a0e59baa09c1
Serezha <volos11223344@yandex.ru>
2023-06-18 20:18:17 +0300

    created random number function

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-'Палиндром';
+//'Палиндром';
 
 function checkPalindrome(string) {
   const newString = string.toLowerCase().replaceAll(' ', '').split('').reverse().join('');
@@ -14,6 +14,37 @@ console.log(checkPalindrome('А роза упала на лапу Азора'));
 console.log(checkPalindrome('Сегодня жарко'));
 console.log(checkPalindrome('Шалаш'));
 
-// let string = 'А роза упала на лару Азора';
-// string = 
-// console.log(string);
+
+//'return positive numbers';
+
+function returnNumbers(string) {
+  let number = string.split(' ').map(item => parseInt(item)).filter(item => !isNaN(item));
+  let [outputNumber] = number;
+  if(!outputNumber) {
+    return NaN;
+  } else {
+    return Math.abs(outputNumber);
+  }
+}
+
+console.log(returnNumbers('2023 год'));            // 2023
+console.log(returnNumbers('ECMAScript 2022'));     // 2022
+console.log(returnNumbers('агент 007'));           // 7
+console.log(returnNumbers('-7'));                  // 7
+console.log(returnNumbers('а я томат'));           // NaN
+
+//'create random number'
+
+function randomNumber(min, max, quantity) {
+  let num = (Math.random() * (max - min + 1)) + min;
+  console.log(num);
+  const string = num.toString();
+  const index = string.indexOf('.') + 1;
+  num = string.slice(index , quantity + index);
+  return num;
+}
+
+
+console.log(randomNumber(1, 10, 5));
+console.log(randomNumber(1, 6, 3));
+console.log(randomNumber(1, 6, 2));
