494219da19a123b6ddca8110bbce3f6e3a77e9ff
Serezha <volos11223344@yandex.ru>
2023-08-30 23:10:01 +0300

    филтрует карту

--- js/modules/create-map.js
+++ js/modules/create-map.js
@@ -112,21 +112,115 @@ const renderPins = (data) => {
 		unlockFilters();
 
 		//обработчик события на изменения в фильтрах
+		// form.addEventListener("change", function (evt) {
+		// 	// очищаем карту от старых маркеров
+		// 	const mapMarkers = document.querySelectorAll(".leaflet-marker-icon");
+		// 	mapMarkers.forEach((item) => {
+		// 		item.remove();
+		// 	});
+
+		// 	//Удаляем открытый попап
+		// 	const popupOpened = document.querySelector(".leaflet-popup");
+		// 	popupOpened.remove();
+
+		// 	const selectedPriceRange = housePrices[evt.target.value];
+		// 	const filteredData = data.filter((item) => {
+		// 		return (
+		// 			item.offer.price >= selectedPriceRange.from &&
+		// 			item.offer.price <= selectedPriceRange.to
+		// 		);
+		// 	});
+		// 	// массив filteredData содержит только те элементы, цена которых попадает в выбранный диапазон
+		// 	console.log(filteredData);
+		// 	console.log(filteredData.length);
+		// 	if (filteredData) {
+		// 		for (let i = 0; i < filteredData.length; i++) {
+		// 			console.log(filteredData[i]);
+		// 			const card = renderInfoBlock(filteredData[i]);
+
+		// 			const lat = data[i].location.lat;
+		// 			const lng = data[i].location.lng;
+		// 			const marker = L.marker(
+		// 				{
+		// 					lat,
+		// 					lng,
+		// 				},
+		// 				{
+		// 					icon,
+		// 				}
+		// 			);
+
+		// 			marker.addTo(map);
+		// 			marker.bindPopup(card);
+		// 		}
+		// 	}
+		// });
+
+		//2ой вариант
 		form.addEventListener("change", function (evt) {
 			// очищаем карту от старых маркеров
-			const leafletMarkers = document.querySelectorAll(".leaflet-marker-icon");
-			leafletMarkers.forEach((item) => {
+			const mapMarkers = document.querySelectorAll(".leaflet-marker-icon");
+			mapMarkers.forEach((item) => {
 				item.remove();
 			});
 
-			const selectedPriceRange = housePrices[evt.target.value];
+			//Удаляем открытый попап
+			const popupOpened = document.querySelector(".leaflet-popup");
+			if (popupOpened) {
+				popupOpened.remove();
+			}
+
+			//Выбираем все выбранные чекбоксы
+			const selectedFeatures = Array.from(
+				document.querySelectorAll(
+					'#housing-features input[type="checkbox"]:checked'
+				)
+			).map((input) => input.value);
+
+			const selectedType = document.querySelector("#housing-type").value;
+			const selectedPrice = document.querySelector("#housing-price").value;
+			const selectedRooms = document.querySelector("#housing-rooms").value;
+			const selectedGuests = document.querySelector("#housing-guests").value;
+
 			const filteredData = data.filter((item) => {
 				return (
-					item.offer.price >= selectedPriceRange.from &&
-					item.offer.price <= selectedPriceRange.to
+					(selectedType === "any" || item.offer.type === selectedType) &&
+					(selectedPrice === "any" ||
+						(selectedPrice === "low" && item.offer.price < 10000) ||
+						(selectedPrice === "middle" &&
+							item.offer.price >= 10000 &&
+							item.offer.price <= 50000) ||
+						(selectedPrice === "high" && item.offer.price > 50000)) &&
+					(selectedRooms === "any" || item.offer.rooms == selectedRooms) &&
+					(selectedGuests === "any" || item.offer.guests == selectedGuests) &&
+					(selectedFeatures.length === 0 ||
+						(item.offer.features &&
+							selectedFeatures.every((feature) =>
+								item.offer.features.includes(feature)
+							)))
 				);
 			});
-			// теперь массив filteredData содержит только те элементы, цена которых попадает в выбранный диапазон
+
+			// const selectedType = document.querySelector("#housing-type").value;
+			// const selectedPrice = document.querySelector("#housing-price").value;
+			// const selectedRooms = document.querySelector("#housing-rooms").value;
+			// const selectedGuests = document.querySelector("#housing-guests").value;
+
+			// const filteredData = data.filter((item) => {
+			// 	return (
+			// 		(selectedType === "any" || item.offer.type === selectedType) &&
+			// 		(selectedPrice === "any" ||
+			// 			(selectedPrice === "low" && item.offer.price < 10000) ||
+			// 			(selectedPrice === "middle" &&
+			// 				item.offer.price >= 10000 &&
+			// 				item.offer.price <= 50000) ||
+			// 			(selectedPrice === "high" && item.offer.price > 50000)) &&
+			// 		(selectedRooms === "any" || item.offer.rooms == selectedRooms) &&
+			// 		(selectedGuests === "any" || item.offer.guests == selectedGuests)
+			// 	);
+			// });
+
+			// массив filteredData содержит только те элементы, которые соответствуют выбранным фильтрам
 			console.log(filteredData);
 			console.log(filteredData.length);
 			if (filteredData) {
